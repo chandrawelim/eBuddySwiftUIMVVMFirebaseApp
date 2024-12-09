e4049ecd18d849456ce43093514c09fa4f536ef2
chandrawelim <chandra.welim@tuntun.co.id>
2024-12-09 21:08:58 +0700

    First commit

---
+++ eBuddySwiftUIMVVMFirebaseApp/eBuddySwiftUIMVVMFirebaseApp/eBuddySwiftUIMVVMFirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eBuddySwiftUIMVVMFirebaseAppApp.swift
+//  eBuddySwiftUIMVVMFirebaseApp
+//
+//  Created by Chandra Welim on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct eBuddySwiftUIMVVMFirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
